5f4309a270f27a5ea16c3453035e8afcf8218ef5
Jimmy <zhijianliu72@gmail.com>
2024-03-05 17:53:38 -0700

    add qr code generating mechanic

---
+++ EventEase2/app/src/main/java/com/example/eventease2/OrganizerQRCodeMaker.java
@@ -0,0 +1,58 @@
+package com.example.eventease2;
+
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import com.example.eventease2.R;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class OrganizerQRCodeMaker extends AppCompatActivity {
+    private ImageView qrCodeImageView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.qr_code);
+
+
+        qrCodeImageView = findViewById(R.id.qr_code_image_view);
+
+
+        generateQRCode("https://example.com");
+    }
+
+
+    private void generateQRCode(String content) {
+        QRCodeWriter writer = new QRCodeWriter();
+        try {
+            BitMatrix bitMatrix = writer.encode(content, BarcodeFormat.QR_CODE, 512, 512);
+            int width = bitMatrix.getWidth();
+            int height = bitMatrix.getHeight();
+            Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    bmp.setPixel(x, y, bitMatrix.get(x, y) ? getResources().getColor(R.color.black) : getResources().getColor(R.color.white));
+                }
+            }
+            qrCodeImageView.setImageBitmap(bmp);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
