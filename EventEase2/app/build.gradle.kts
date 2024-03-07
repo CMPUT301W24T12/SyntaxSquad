6b6364c4d513e93ce21518b39524d39fbf058ce8
Jimmy <zhijianliu72@gmail.com>
2024-03-06 21:19:59 -0700

    Revert "Merge branch 'Ashlyn3' into feature/createQRCode"
    This reverts commit 4ed2aeec05f2db23f4d3c3fec2bf171925f5a4d1, reversing
    changes made to 2c7b71f2108d517da7dd7153519cedf8ca357106.

--- EventEase2/app/build.gradle.kts
+++ EventEase2/app/build.gradle.kts
@@ -41,7 +41,7 @@ dependencies {
     implementation ("com.google.zxing:core:3.4.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation(platform("com.google.firebase:firebase-bom:32.7.3"))
+    implementation(platform("com.google.firebase:firebase-bom:32.7.1"))
     implementation("com.google.firebase:firebase-firestore")
     implementation("com.google.firebase:firebase-storage:20.3.0")
     testImplementation("junit:junit:4.13.2")
