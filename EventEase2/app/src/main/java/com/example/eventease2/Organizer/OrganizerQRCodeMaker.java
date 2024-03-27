5881e8c6f4acc1293b4dbcae05d15bf63f1b87bf
Jimmy <zhijianliu72@gmail.com>
2024-03-27 00:55:47 -0600

    Make check in QR code

--- EventEase2/app/src/main/java/com/example/eventease2/Organizer/OrganizerQRCodeMaker.java
+++ EventEase2/app/src/main/java/com/example/eventease2/Organizer/OrganizerQRCodeMaker.java
@@ -3,13 +3,20 @@ package com.example.eventease2.Organizer;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.os.Environment;
 
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -36,4 +43,31 @@ public class OrganizerQRCodeMaker {
         }
 
     }
+
+//    /**
+//     * generate a qr code that link the user to github download app page if the user does not have the app installed
+//     * link to the app if the app is downloaded
+//     * @return
+//     */
+//    public static Bitmap generateAppQRCode() {
+//        QRCodeWriter writer = new QRCodeWriter();
+//        try {
+//            String content = "https://github.com/CMPUT301W24T12/SyntaxSquad/releases";
+//            BitMatrix bitMatrix = writer.encode(content, BarcodeFormat.QR_CODE, 512, 512);
+//            int width = bitMatrix.getWidth();
+//            int height = bitMatrix.getHeight();
+//            Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+//            for (int x = 0; x < width; x++) {
+//                for (int y = 0; y < height; y++) {
+//                    bmp.setPixel(x, y, bitMatrix.get(x, y) ? Color.BLACK : Color.WHITE);
+//                }
+//            }
+//            return bmp;
+//        } catch (WriterException e) {
+//            e.printStackTrace();
+//            return null;
+//        }
+//
+//    }
+
 }
