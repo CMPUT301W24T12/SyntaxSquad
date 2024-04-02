0c1defca68a0a3ea0d2bbd99d1b32225f63b7760
sepehr <sepehrbehroozi@gmail.com>
2024-04-01 23:56:07 -0600

    I fixed up dependancies in gradle app file

--- EventEase2/app/build.gradle.kts
+++ EventEase2/app/build.gradle.kts
@@ -48,7 +48,7 @@ dependencies {
     implementation("com.google.firebase:firebase-storage:20.3.0")
     implementation ("com.firebaseui:firebase-ui-storage:7.2.0")
     implementation("androidx.activity:activity:1.8.0")
-    implementation("com.google.android.gms:play-services-maps:18.2.0"
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
